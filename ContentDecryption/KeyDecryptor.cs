dd4ceffaf8a69e9bd188fe28e6a2a51b668563b4
Peter Ciszewski <piotrci@microsoft.com>
2019-08-06 12:04:21 -0700

    Added memory cache for RSA providers.

--- ContentDecryption/KeyDecryptor.cs
+++ ContentDecryption/KeyDecryptor.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +11,39 @@ namespace ContentDecryption
 {
     public static class KeyDecryptor
     {
+        static KeyDecryptor()
+        {
+            NameValueCollection cacheConfig = new NameValueCollection();
+            cacheConfig.Add("cacheMemoryLimitMegabytes", "2");
+            rsaProviderCache = new MemoryCache("rsaProviderCache", cacheConfig);
+        }
+        private static readonly MemoryCache rsaProviderCache;
         public static byte[] DecryptSymmetricKey(byte[] encryptedSymmetricKey, string privateKeyId)
         {
-            string privateKey = DummyKeyStore.GetPrivateKey(privateKeyId);
-            using (RSACryptoServiceProvider crypto = new RSACryptoServiceProvider())
+            var crypto = GetProvider(privateKeyId);
+            return crypto.Decrypt(encryptedSymmetricKey, true);
+        }
+        private static RSACryptoServiceProvider GetProvider(string privateKeyId)
+        {
+            RSACryptoServiceProvider provider = rsaProviderCache[privateKeyId] as RSACryptoServiceProvider;
+            if (provider == null)
             {
-                crypto.FromXmlString(privateKey);
-                return crypto.Decrypt(encryptedSymmetricKey, true); 
+                var policy = new CacheItemPolicy
+                {
+                    RemovedCallback = DisposeRemovedProvider,
+                    SlidingExpiration = TimeSpan.FromMinutes(5)
+                };
+                string privateKey = DummyKeyStore.GetPrivateKey(privateKeyId);
+                RSACryptoServiceProvider newProvider = new RSACryptoServiceProvider();
+                newProvider.FromXmlString(privateKey);
+
+                provider = (RSACryptoServiceProvider)rsaProviderCache.AddOrGetExisting(privateKeyId, newProvider, policy) ?? newProvider;
             }
+            return provider;
+        }
+        private static void DisposeRemovedProvider(CacheEntryRemovedArguments args)
+        {
+            ((RSACryptoServiceProvider)args.CacheItem.Value).Dispose();
         }
     }
 }
